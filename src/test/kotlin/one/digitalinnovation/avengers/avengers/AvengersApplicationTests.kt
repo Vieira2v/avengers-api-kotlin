25ea0f67147fec2af96809bbc3b9de09ea7df267
Bruno Vieira <brunovws77@gmail.com>
2024-10-04 10:20:42 -0300

    First commit

---
+++ src/test/kotlin/one/digitalinnovation/avengers/avengers/AvengersApplicationTests.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.avengers.avengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
